30f46188b2d3b316ab8de144c620e5d98cdd1d11
TRISTAN BELDAD <beldadtristandave_bsit@plmun.edu.ph>
2025-02-17 03:44:39 +0800

    Update if_statement_basic.py

--- if_statement_basic.py
+++ if_statement_basic.py
@@ -1,1 +1,20 @@
+temperature = 35
+
+if temperature > 30:
+    print("It's a hot day!")
+
+if_else_statement.py
+
+
+try:
+    user_input = input("Enter a number: ")
+    number = int(user_input)  # Convert input to integer
+
+    if number % 2 == 0:
+        print("The number", number, "is Even.")
+    else:
+        print("The number", number, "is Odd.")
+
+except ValueError:
+    print("Invalid input. Please enter an integer.")
 
